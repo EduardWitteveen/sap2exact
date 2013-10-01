0ed2ee4717ed64bd710faeb9a3636df470440a8b
eywitteveen@gmail.com <eywitteveen@gmail.com@62027631-5072-b850-0c13-7c861fcd3ad2>
2013-10-01 21:51:48 +0000

    - bla bla bla

---
+++ source/sap2exact/sap2exact.Domain/BaseArtikel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access2exact.Domain
+{
+    public class BaseArtikel
+    {
+        public string Code;
+        public string Verpakking;
+        //public string Statistical = "35019090";
+        public string Description;
+        public Dictionary<int, string> Descriptions = new Dictionary<int, string>();
+        
+        //public string VerpakkingType;
+        //public string VerpakkingEenheid;
+        //public double VerpakkingBruto;
+        //public double VerpakkingNetto;
+
+        public string PrijsEenheid;
+        public double PrijsKost;
+        public double PrijsVerkoop;
+        public double PrijsGewichtNetto;
+
+        // uit scherm arikelen per leverancier
+        public string VerkoopVerpakking;
+        public double VerkoopAantalNetto;
+        public double VerkoopAantalBruto;
+        public string VerkoopGewichtEenheid;
+        public string TimeStamp;
+    }
+}
---
+++ source/sap2exact/sap2exact.Domain/ExportData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace access2exact.Domain
+{
+    public class ExportData        
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual Dictionary<string, Domain.EindArtikel> EindArtikelen { get; set; }
+        public virtual Dictionary<string, Domain.ReceptuurArtikel> ReceptuurArtikelen { get; set; }
+        public virtual Dictionary<string, Domain.VerpakkingsArtikel> VerpakkingsArtikelen { get; set; }
+        public virtual Dictionary<string, Domain.GrondstofArtikel> GrondstofArtikelen { get; set; }
+        public virtual Dictionary<string, Domain.IngredientArtikel> IngredientArtikelen { get; set; }
+
+        public ExportData()
+        {
+            EindArtikelen = new Dictionary<string, Domain.EindArtikel>();
+            ReceptuurArtikelen = new Dictionary<string, Domain.ReceptuurArtikel>();
+            VerpakkingsArtikelen = new Dictionary<string, Domain.VerpakkingsArtikel>();
+            GrondstofArtikelen = new Dictionary<string, Domain.GrondstofArtikel>();
+            IngredientArtikelen = new Dictionary<string, Domain.IngredientArtikel>();
+        }
+
+
+        public virtual void Add(BaseArtikel artikel)
+        {
+            if(artikel.GetType() == typeof(EindArtikel)) {
+                //if(EindArtikelen.ContainsKey(artikel.Code)) EindArtikelen.Add(artikel.Code, (EindArtikel)artikel);
+                EindArtikelen.Add(artikel.Code, (EindArtikel)artikel);
+            }
+            else if(artikel.GetType() == typeof(ReceptuurArtikel)) {
+                if (!ReceptuurArtikelen.ContainsKey(artikel.Code)) ReceptuurArtikelen.Add(artikel.Code, (ReceptuurArtikel)artikel);
+            }
+            else if(artikel.GetType() == typeof(VerpakkingsArtikel)) {
+                if (!VerpakkingsArtikelen.ContainsKey(artikel.Code)) VerpakkingsArtikelen.Add(artikel.Code, (VerpakkingsArtikel)artikel);
+            }
+            else if(artikel.GetType() == typeof(GrondstofArtikel)) {
+                if (!GrondstofArtikelen.ContainsKey(artikel.Code)) GrondstofArtikelen.Add(artikel.Code, (GrondstofArtikel)artikel);
+            }
+            else if (artikel.GetType() == typeof(IngredientArtikel))
+            {
+                if (!IngredientArtikelen.ContainsKey(artikel.Code)) IngredientArtikelen.Add(artikel.Code, (IngredientArtikel)artikel);
+            }
+            else throw new NotImplementedException("unknown type: " + artikel.GetType().FullName);
+        }
+    }
+}
