a0bbb3b2ef3c3c6823a86c5c62690dfa98206fc6
eywitteveen@gmail.com <eywitteveen@gmail.com@62027631-5072-b850-0c13-7c861fcd3ad2>
2013-10-19 15:30:25 +0000

    - it works because of herbs

---
+++ trunk/source/sap2exact/sap2exact/Output.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace access2exact
+{
+    public static class Output
+    {
+        private class ErrorInfo
+        {
+            private FileInfo errorlog;
+            public ErrorInfo()
+            {
+                errorlog = new FileInfo("error.txt");
+                if (errorlog.Exists) errorlog.Delete();
+            }
+            public void Write(string message) {
+                var writer = errorlog.AppendText();
+                writer.WriteLine(message);
+                writer.Close();
+            }
+            
+        }
+        private static ErrorInfo errorinfo = new ErrorInfo();
+
+        public static void Info(string message)
+        {
+            System.Diagnostics.Debug.WriteLine("[OUTPUT INFO] " + message);
+            Console.Out.WriteLine(message);
+        }
+
+        public static void Error(string message)
+        {
+            errorinfo.Write(message);
+            System.Diagnostics.Debug.WriteLine("[OUTPUT ERROR] " + message);
+            Console.Error.WriteLine(message);
+        }
+    }
+}
